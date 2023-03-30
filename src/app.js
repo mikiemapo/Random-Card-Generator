b5a8d159f964cb44d50f3ff2d1c44d9f15b13f19
mikiemapo <findmikemm@gmail.com>
2023-03-30 01:33:56 +0000

    final code with random function color update resize and margin

--- src/app.js
+++ src/app.js
@@ -4,8 +4,39 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+// write your code here
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomMid = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "K",
+    "Q",
+    "A"
+  ];
+  // randomMid.forEach(item => console.log(item));
+  let randomMidIndex = Math.floor(Math.random() * randomMid.length);
+  let midPart = randomMid[randomMidIndex];
+
+  // suit.forEach(item => console.log(item));
+  let suit = ["♦", "♥", "♠", "♣"];
+  let randSuitIndex = Math.floor(Math.random() * suit.length);
+  let suitPart = suit[randSuitIndex];
+
+  if (suitPart == "♦" || suitPart == "♥") {
+    document.querySelectorAll(".suit-color").forEach(item => {
+      item.style.color = "red";
+    });
+  }
+
+  document.getElementById("suitTop").innerHTML = suitPart;
+  document.getElementById("suitBot").innerHTML = suitPart;
+  document.getElementById("midFace").innerHTML = midPart;
 };
