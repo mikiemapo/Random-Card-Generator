7d4f18de01c9145b139c4d92904445ad5f3483a5
mikiemapo <findmikemm@gmail.com>
2023-03-30 22:07:26 +0000

    second card with diff face

--- src/app.js
+++ src/app.js
@@ -24,11 +24,15 @@ window.onload = function() {
   // randomMid.forEach(item => console.log(item));
   let randomMidIndex = Math.floor(Math.random() * randomMid.length);
   let midPart = randomMid[randomMidIndex];
+  let randomMidIndex1 = Math.floor(Math.random() * randomMid.length);
+  let midPart1 = randomMid[randomMidIndex1];
 
   // suit.forEach(item => console.log(item));
   let suit = ["♦", "♥", "♠", "♣"];
   let randSuitIndex = Math.floor(Math.random() * suit.length);
   let suitPart = suit[randSuitIndex];
+  let randSuitIndex1 = Math.floor(Math.random() * suit.length);
+  let suitPart1 = suit[randSuitIndex1];
 
   if (suitPart == "♦" || suitPart == "♥") {
     document.querySelectorAll(".suit-color").forEach(item => {
@@ -36,7 +40,17 @@ window.onload = function() {
     });
   }
 
+  if (suitPart1 == "♦" || suitPart1 == "♥") {
+    document.querySelectorAll(".suit-color1").forEach(item => {
+      item.style.color = "red";
+    });
+  }
+
   document.getElementById("suitTop").innerHTML = suitPart;
   document.getElementById("suitBot").innerHTML = suitPart;
   document.getElementById("midFace").innerHTML = midPart;
+
+  document.getElementById("suitTop1").innerHTML = suitPart1;
+  document.getElementById("suitBot1").innerHTML = suitPart1;
+  document.getElementById("midFace1").innerHTML = midPart1;
 };
